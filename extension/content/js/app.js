16385ad85c5c67a32e8e82d48d9762be3cee08df
Michael Donohoe <donohoe@gmail.com>
2017-12-01 17:45:27 -0500

    Major update to move past initial roots

--- extension/content/js/app.js
+++ extension/content/js/app.js
@@ -1,14 +1,127 @@
 var wow = new WOW({});
 var app = {
 
+	sources: {
+		newyorker: {
+			label: "The New Yorker",
+			dek:   "Photo Booth",
+			feed:  "newyorker.js",
+			icon:  "newyorker.svg",
+			link:  "https://www.newyorker.com/culture/photo-booth",
+			subs:  "https://www.newyorker.com/subscribe"
+		},
+		nytimes: {
+			label: "The New York Times",
+			dek:   "Lens",
+			feed:  "nytimes.js",
+			icon:  "nytimes.svg",
+			link:  "https://lens.blogs.nytimes.com",
+			subs:  "https://www.nytimes.com/subscription/multiproduct/lp8XKUR.html"
+		},
+		natgeo: {
+			label: "National Geographic",
+			dek:   "Photo of the Day",
+			feed:  "nationalgeographic.js",
+			icon:  "nationalgeographic.svg",
+			link:  "https://www.nationalgeographic.com/photography/photo-of-the-day/",
+			subs:  "https://www.nationalgeographic.com/subscribe/magazines/"
+		},
+	},
+
 	run: function() {
-		console.log("Photobooth - The New Yorker (Chrome brwoser extension)");
+		console.log("Photobooth (Chrome browser extension)");
+		this.setup();
 		if (window.navigator.onLine) {
 			this.requestData();
 		} else {
 			setTimeout(function() {
+				app.haiku();
 				document.body.className = "offline";
 			}, 10);
+		}
+	},
+
+	setup: function() {
+		this.currentSource  = this.getSource();
+		this.allowedSources = Object.keys(app.sources);
+		this.setupDropDown();
+		this.setupFooter();
+	},
+
+	setupFooter: function() {
+		var html = "";
+		var len = this.allowedSources.length;
+		for (var i=0; i<len; i++) {
+			var k = this.allowedSources[i];
+			var label = this.sources[k].label;
+			var dek   = this.sources[k].dek;
+			var link  = this.sources[k].link;
+			var subs  = this.sources[k].subs;
+			html += "<div><a href=\"" + link + "\">" + label + ": " + dek + "</a> <a class=\"subs\" href=\"" + subs + "\">Subscribe</a></div>";
+		}
+		document.getElementById("footer-links").innerHTML = html;
+	},
+
+	updateModifiedDate: function(data) {
+		if (data.status) {
+			var monthNames = [
+				"January", "February", "March", "April", "May", "June",
+				"July", "August", "September", "October", "November", "December"
+			];
+
+			var d = new Date(data.status.updated);
+			var updated = 'm d Y'
+				.replace('Y', d.getFullYear())
+				.replace('m', monthNames[d.getMonth()])
+				.replace('d', d.getDate());
+
+			var h = "Updated: " + updated;
+			h += "&nbsp;<a href='https://github.com/donohoe/photo-booth/issues'>Report a problem</a>"
+			document.getElementById("version").innerHTML = h;
+		}
+	},
+
+	updateSourceLabel: function(){
+		var el = document.getElementById("source-name");
+		el.innerHTML = this.sources[this.currentSource].label;
+		if (el.className === "") {
+			el.className = "show";
+		}
+	},
+
+	setupDropDown: function() {
+		var that = this;
+		var html = "";
+		var el = document.getElementById("dd");
+
+		var len = this.allowedSources.length;
+		for (var i=0; i<len; i++) {
+			var k = this.allowedSources[i];
+			var l = this.sources[ k ].label;
+			html += "<li data-source=\"" + k + "\" data-label=\"" + l + "\"><a href=\"#\"><i class=\"icon icon-" + k + "\"></i>" + l + "</a></li>";
+		}
+		document.getElementById("source-list").innerHTML = html;
+
+		el.addEventListener('click', function() {
+			if (this.className.indexOf("active") > -1) {
+				this.className = this.className.replace("active", "").trim();
+			} else {
+				this.className += " active";
+			}
+			return false;
+		});
+
+		var opts = el.querySelectorAll('ul > li');
+		var len = opts.length;
+		for (var i=0; i<len; i++) {
+			opts[i].addEventListener('click', function() {
+				var chosenSource = this.getAttribute("data-source");
+				if (chosenSource !== that.currentSource) {
+					that.setSource(chosenSource);
+					that.requestData();
+				}
+				return false;
+			});
 		}
 	},
 
@@ -16,10 +129,8 @@ var app = {
 		var items = data.response || false;
 		if (items) {
 
-			this.scrollBarWidth = this.measureScrollBar();
-
 			var html = "";
-			var realWidth = (window.innerWidth - this.scrollBarWidth);
+			var realWidth = window.innerWidth;
 			var numPerRow = parseInt(realWidth / 250, 10);
 			var w = parseInt((realWidth / numPerRow), 10) + "px";
 			var h = w;
@@ -48,9 +159,12 @@ var app = {
 			this.loadImages();
 
 			wow.init();
+			window.scrollTo(0, 1);
 
 			document.body.className = "loaded";
 			this.addEvents();
+			this.updateSourceLabel();
+			this.updateModifiedDate(data);
 		}
 	},
 
@@ -80,15 +194,20 @@ var app = {
 		var effect = this.randomEffect();
 		return [
 			'<a data-wow-offset="' + offset + '" data-wow-duration="' + duration + '" class="wow link ' + effect + '" href="' + item.link + '" target="_blank" style="width: '+ w +'; height: '+ h +';">',
-				'<img class="' + item.orientation + '" data-src="' + item.image + '" style="min-width: ' + w + 'px;" title="' + item.title + '">',
+			'<img class="' + item.orientation + '" data-src="' + item.image + '" style="min-width: ' + w + 'px;" data-title="' + item.title + '">',
 			'</a>'
 		].join("");
 	},
 
 	addEvents: function() {
+		if (this.eventsAdded) {
+			return;
+		}
 		var self = this;
+		self.eventsAdded = true;
+
 		window.addEventListener('resize', function(event) {
-			var realWidth = (window.innerWidth - self.scrollBarWidth);
+			var realWidth = window.innerWidth;
 			var numPerRow = parseInt(realWidth / 250, 10);
 			var w = parseInt((realWidth / numPerRow), 10) + "px";
 			var links = document.querySelectorAll("a.link");
@@ -99,20 +218,8 @@ var app = {
 			}
 		});
 
-		var contentHeight = document.getElementById("content").offsetHeight;
-		window.onscroll = function (event) {
-			var scrollPosition = window.pageYOffset;
-			if (window.pageYOffset % 15 == 0) {
-				if (contentHeight - scrollPosition < 803) {
-					document.getElementById("button").className = "bottom";
-				} else {
-					document.getElementById("button").className = "";
-				}
-			}
-		}
-
 		setTimeout(function() {
-			document.getElementById("footer").style.display = "block";
+			document.getElementById("footer").style.opacity = 1;
 		}, 2201); // If you deviate from 2201 then kittens die
 	},
 
@@ -126,6 +233,7 @@ var app = {
 	},
 
 	requestData: function() {
+
 		var responseText = this.getFromCache();
 		if (responseText) {
 			this.buildTheDamnThing(JSON.parse(responseText));
@@ -145,40 +253,71 @@ var app = {
 			}
 		}
 
-	//	A seperate service pulls data and drops it as JSON. Source must be on HTTPS and declared in the manifest.
-		xmlhttp.open("GET", "https://donohoe.io/project/feeds/photo-booth/feed.js", true);
-		xmlhttp.send();
+		var feed = this.sources[this.currentSource].feed || false;
+		if (feed) {
+		//	A seperate service pulls data and drops it as JSON. Source must be on HTTPS and declared in the manifest.
+			xmlhttp.open("GET", "https://donohoe.io/project/feeds/photo-booth/" + feed, true);
+			xmlhttp.send();
+		}
+	},
+
+	getSource: function() {
+		return localStorage.getItem("photobooth-source") || "newyorker";
+	},
+
+	setSource: function(source) {
+		if (this.allowedSources.indexOf(source) > -1) {
+			localStorage.setItem("photobooth-source", source);
+			this.currentSource = source;
+		}
 	},
 
 	getFromCache: function() {
-		var data = JSON.parse(localStorage.getItem("photobooth-data")) || false;
+		var key = "photobooth-data-" + this.currentSource;
+		var data = JSON.parse(localStorage.getItem(key)) || false;
 		if (!data) {
 			return false;
 		}
-
 		var expiration = data.expiration;
 		var now = new Date().getTime();
 		if (now > expiration) {
 			return false;
 		}
-
 		return data.responseText || false;
 	},
 
 	saveToCache: function(response) {
-		localStorage.setItem("photobooth-data", JSON.stringify({
+		var key = "photobooth-data-" + this.currentSource;
+		localStorage.setItem(key, JSON.stringify({
 			expiration: new Date().getTime() + 6 * 60 * 60 * 1000, // 6 hours
 			responseText: response
 		}));
 	},
 
-	measureScrollBar: function() {
-		var scrollDiv = document.createElement("div");
-		scrollDiv.className = "scrollbar-measure";
-		document.body.appendChild(scrollDiv);
-		var scrollBarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-		document.body.removeChild(scrollDiv);
-		return scrollBarWidth || 15;
+	haiku: function() {
+		var hs = [
+			"when you talk to me // time is no longer lonely // night seems like a day",
+			"you’re an awesome friend // a sister to me as well // two-in-one god’s gift",
+			"your knowledge and skills // always unselfishly shared // your praise: mind tonic",
+			"my inspiration // in all my writing passions // you have touched my life",
+			"moments that we have // be kept in my treasure box // i thank you so much",
+			"may our friendship lasts // as i keep you in my heart // forever you’re loved",
+			"sharing is friendship. // moon and sun are best of friends. // they take turns to shine",
+			"explosion of light // pooled the bay with reds and gold // catching fishermen",
+			"Falcon at light speed // wit banter swagger shoot first // carbonite classic",
+			"We are no strangers // Never gonna give you up // Ain't gonna let go",
+			"C-Beams in the dark // Ships burning near Orion // Lost like tears in rain",
+			"A world with two suns // You'd think they'd have two shadows // What's up, Tatooine?",
+			"If time is money // Are ATMs time machines? // our mind has been blown",
+			"1981 // I'm into Space Invaders // 2600",
+			"This tiny haiku // is just sixty characters // ideal for Twitter.",
+			"When I read haiku, // I hear it in the voice of // William Shatner."
+		];
+		var el = document.getElementById("offline");
+		var h = hs[Math.floor(Math.random()*hs.length)];
+		h = h.replace(/ \/\/ /g, ' <br/> ');
+		el.innerHTML = h;
+		el.style.display = "block";
 	}
 };
 
